8995972ad46ad21b7ac48b4550c0fc3cc65e544d
Kenzothd <Kenzothd@hotmail.com>
2023-02-08 16:05:29 +0800

    psycopg reconnect fix 2

--- app.py
+++ app.py
@@ -22,19 +22,16 @@ app.register_blueprint(alerts, url_prefix="/alerts")
 # cors = CORS(app, resources={r'*': {'origins': 'http://localhost:3000'}})
 
 url = os.environ.get("DATABASE_URL")  # gets variables from environment
+
 connection = None
-for i in range(10):
+while True:
     try:
-        psycopg2.connect(url)
-        break
+        connection = psycopg2.connect(url)
+        # perform database operations
+        time.sleep(1800)  # wait for 30 minutes before reconnecting
     except psycopg2.OperationalError as e:
         print("Cannot connect to database, retrying in 5 seconds...")
         time.sleep(5)
-if connection:
-    # perform database operations
-    connection.close()
-else:
-    print("Unable to connect to the database")
 
 
 @app.route("/", methods=["GET", "POST"])
